218afef7f1da0febe3bca468237f9547b802741b
DhrumilShah98 <shah.dhrumil1998@gmail.com>
2021-06-24 01:22:21 +0530

    Tutorial 3 code in tutorial_3 branch

--- tutorial_4/src/apis/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'https://tutorial4-api.herokuapp.com/api/users' });
-
-export const performLogin = (loginData) => API.post('/login', loginData);
-
-export const getAllUsers = () => API.get('/');
-
-export const getUserDetails = (userId) => API.get(`/${userId}`);
